6acfdc5bf2f1927912c1b2a51d56fec340fcb533
zhongcs123 <947426700@qq.com>
2019-06-12 17:42:54 +0800

    测试版本V1.0.1

---
+++ src/test/java/HttpClient/HttpClientSpringboot/testCase/NewTest.java
@@ -0,0 +1,20 @@
+package HttpClient.HttpClientSpringboot.testCase;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("test");
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
